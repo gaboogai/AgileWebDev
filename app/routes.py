6bbb54096b1285fd76e4e5976c9a38bf57385a36
gaboogai <michael.james.millar@gmail.com>
2025-05-01 19:22:22 +0800

    Login.html moved into templates folder and added as route /login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
-@app.route('/')
+
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
@@ -18,8 +18,18 @@ def index():
 
 @app.route('/button')
 def button():
-    return render_template("button.html", title="Button!")
+    return render_template("button.html", title="Button")
 
 @app.route('/share')
 def share():
     return render_template("share.html", title="Share")
+
+@app.route('/')
+def home():
+    #TODO: check if user is logged in, if so redirect to dashboard
+    #else redirect to login
+    return redirect('/login')
+
+@app.route('/login')
+def login_Register():
+    return render_template("login.html", title="Login/Register")
