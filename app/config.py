30a74b46ac1e94b8ff027b9e61c66aa95cdd0940
samo-e <guptasamay@gmail.com>
2025-05-02 18:47:39 +0800

    updated class config

--- app/config.py
+++ app/config.py
@@ -6,3 +6,4 @@ default_database_location =  'sqlite:///' + os.path.join(base_dir, 'app.db')
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
     SECRET_KEY = "you-will-never-guess"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
