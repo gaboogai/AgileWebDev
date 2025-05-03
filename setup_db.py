19c6e10f9835dd33c5e437da6822480b666989d9
samo-e <guptasamay@gmail.com>
2025-05-03 22:52:00 +0800

    alternate testing by removing flask migrate dependency

--- setup_db.py
+++ setup_db.py
@@ -1,7 +1,15 @@
-from app import app, db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'app', 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 from app.models import User, Song, Review
 
 with app.app_context():
     db.create_all()
-    
     print("Database tables created.")
