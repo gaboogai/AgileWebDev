73368365fde4f9b337a6cd233ebfd9a3101d3cda
samo-e <guptasamay@gmail.com>
2025-05-03 23:05:46 +0800

    changing config.py

--- app/config.py
+++ app/config.py
@@ -1,9 +1,8 @@
 import os
 
-base_dir = os.path.abspath(os.path.dirname(__file__))
-default_database_location =  'sqlite:///' + os.path.join(base_dir, 'app.db')
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
-    SECRET_KEY = "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = 'your-secret-key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
