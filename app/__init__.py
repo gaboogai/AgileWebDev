674d027e5e9248c47f85d9e22e13d6225688bc04
samo-e <guptasamay@gmail.com>
2025-05-03 18:23:58 +0800

    updated innit to import db models

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,9 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import models, routes
+from app import routes, models
+
+from app.models import User, Song, Review
 
 from app.commands import init_db_command, seed_db_command
 app.cli.add_command(init_db_command)
